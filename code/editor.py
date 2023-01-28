99bf77eaa0d7f23a5943ddc9aece5d494cfb47bb
Junior <eecfredes@gmail.com>
2023-01-28 20:48:19 -0300

    :sparkles: feat: The red dot

--- code/editor.py
+++ code/editor.py
@@ -1,23 +1,26 @@
 import pygame
+from pygame.math import Vector2 as vector
 import sys
 from settings import *
 
 
 class Editor:
     def __init__(self):
-
         # main setup
         self.display_surface = pygame.display.get_surface()
 
+        # Navigation
+        self.origin = vector()
+
     def event_loop(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+        # event loop
+        # close the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
     def run(self, dt):
         self.display_surface.fill('white')
         self.event_loop()
-
-
+        pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
