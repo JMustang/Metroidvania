8d69a7085f55b620ff2a85496f9774df1f875569
Junior <eecfredes@gmail.com>
2023-01-28 21:15:33 -0300

    :sparkles: feat: Moviment event

--- code/editor.py
+++ code/editor.py
@@ -1,5 +1,7 @@
 import pygame
 from pygame.math import Vector2 as vector
+from pygame.mouse import get_pressed as mouse_buttons
+from pygame.mouse import get_pos as mouse_pos
 import sys
 from settings import *
 
@@ -11,7 +13,9 @@ class Editor:
 
         # Navigation
         self.origin = vector()
+        self.pan_active = False
 
+    # INPUT SECTION
     def event_loop(self):
         # event loop
         # close the game
@@ -19,6 +23,18 @@ class Editor:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            self.pan_input(event)
+
+    def pan_input(self, event):
+        # Middle mouse button presses / released
+        if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[1]:
+            self.pan_active = True
+        if not mouse_buttons()[1]:
+            self.pan_active = False
+
+        # panning update
+        if self.pan_active:
+            self.origin = mouse_pos()
 
     def run(self, dt):
         self.display_surface.fill('white')
