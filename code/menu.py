6bac5f8a144f45d0b70be65386c46830e0b51d0a
Junior <eecfredes@gmail.com>
2023-02-04 16:07:14 -0300

    :sparkles: feat: generic button

--- code/menu.py
+++ code/menu.py
@@ -8,11 +8,15 @@ class Menu:
 
     def create_button(self):
 
-        # menu area general
+        # menu area
         size = 180
         margin = 6
         topLeft = (WINDOW_WIDTH - size - margin, WINDOW_HEIGHT - size - margin)
         self.rect = pygame.Rect(topLeft, (size, size))
 
+        # Button area
+        self.generic_button_rect = pygame.Rect(self.rect.topleft, (self.rect.width / 2, self.rect.height / 2))
+
     def display(self):
         pygame.draw.rect(self.display_surface, 'red', self.rect)
+        pygame.draw.rect(self.display_surface, 'yellow', self.generic_button_rect)
