abaf4c2c3b208df19a09859734f295c2c6220811
Junior <eecfredes@gmail.com>
2023-02-04 21:35:27 -0300

    :sparkles: feat: four button create

--- code/menu.py
+++ code/menu.py
@@ -15,8 +15,20 @@ class Menu:
         self.rect = pygame.Rect(topLeft, (size, size))
 
         # Button area
-        self.generic_button_rect = pygame.Rect(self.rect.topleft, (self.rect.width / 2, self.rect.height / 2))
+        generic_button_rect = pygame.Rect(self.rect.topleft, (self.rect.width / 2, self.rect.height / 2))
+        button_margin = 5
+        self.tile_button_rect = generic_button_rect.copy().inflate(-button_margin, -button_margin)
+        self.coin_button_rect = generic_button_rect.move(self.rect.height / 2, 0).inflate(
+            -button_margin, -button_margin
+            )
+        self.enemy_button_rect = generic_button_rect.move(self.rect.height / 2, self.rect.width / 2).inflate(
+            -button_margin, -button_margin
+            )
+        self.palm_button_rect = generic_button_rect.move(0, self.rect.width / 2).inflate(-button_margin, -button_margin)
 
     def display(self):
-        pygame.draw.rect(self.display_surface, 'red', self.rect)
-        pygame.draw.rect(self.display_surface, 'yellow', self.generic_button_rect)
+        # pygame.draw.rect(self.display_surface, 'red', self.rect)
+        pygame.draw.rect(self.display_surface, 'yellow', self.tile_button_rect)
+        pygame.draw.rect(self.display_surface, 'blue', self.coin_button_rect)
+        pygame.draw.rect(self.display_surface, 'brown', self.enemy_button_rect)
+        pygame.draw.rect(self.display_surface, 'purple', self.palm_button_rect)
